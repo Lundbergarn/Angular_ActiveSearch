da2d9700ff405fda5b13ec260810714e4002aafb
Christoffer Lundberg <lundbergarn2@hotmail.com>
2020-01-02 18:28:48 +0100

    init searchfunction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { debounceTime, filter, distinct } from 'rxjs/operators';
+import { Subscription } from "rxjs";
+
+interface Items {
+    total_count: number,
+    incomplete_results: boolean,
+    items: Array<object>
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-search';
+export class AppComponent implements OnDestroy{
+  title="Instant Search"
+  searchTerm: string;
+  latestSearch = new Subject<string>();
+  results: Array<object>;
+
+  private subscription: Subscription;
+  
+  constructor(public http: HttpClient) {
+    this.subscription = this.latestSearch.pipe(
+      debounceTime(500),
+      distinct(),
+      filter(res => !!res)
+      )
+      .subscribe(term => {
+        this.http.get<Items>('https://api.github.com/search/repositories?q=' + term)
+        .subscribe(res => {
+          this.results = res.items;
+          console.log(res)
+        });
+      }
+    )
+  }
+  
+  newSearch(term) {
+    this.latestSearch.next(term);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatInputModule } from '@angular/material';
+import {MatListModule} from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +16,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
